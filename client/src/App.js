6fd17eaee1516407e87e3a6f8a964d6b789f55f9
Mohamed <mhassankd35@gmail.com>
2022-12-06 23:43:12 -0600

    adds some routing to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,18 @@
+import "./index.css";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import Landing from "./pages/Landing";
+
 function App() {
-  return <div className="App"></div>;
+  const navigate = useNavigate();
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        {/* <Route path="/settings" element={<Settings />} /> */}
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
