bc650636c7f7faadb9f458b98d2c9b71a0c88ef5
Mohamed <mhassankd35@gmail.com>
2022-12-08 03:46:42 -0600

    imports user context and adds dashboard page routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import "./index.css";
 import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
+import { userContext } from "./provider/userProvider";
 import Landing from "./pages/Landing";
+import Dashboard from "./pages/Dashboard";
+import SignUp from "./components/Landing/SignUp";
 
 function App() {
+  const { user, setUser } = useContext(userContext);
+
   const navigate = useNavigate();
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<Landing />} />
-        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         {/* <Route path="/settings" element={<Settings />} /> */}
       </Routes>
     </div>
