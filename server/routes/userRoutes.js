f655edf08a5321a029b93f1ef1c375d659f2e65d
Mohamed <mhassankd35@gmail.com>
2022-12-08 03:49:42 -0600

    adds token verification middleware to private routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
-
+const { verifyJWT } = require("../Middleware/authMiddleware");
 const bcrypt = require("bcryptjs");
 const {
   registerUser,
   loginUser,
+  getMe,
   updateUser,
   deleteUser,
 } = require("../controllers/userControllers");
 
 router.post("/login", loginUser);
 router.post("/register", registerUser);
-
+router.get("/me", verifyJWT, getMe);
 module.exports = router;
