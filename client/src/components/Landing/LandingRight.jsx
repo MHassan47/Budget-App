c0262d2b8e58688aee012077f9a37afd50eecf92
Mohamed <mhassankd35@gmail.com>
2022-12-07 19:09:36 -0600

    imports button component for rerouting to signup page

--- client/src/components/Landing/LandingRight.jsx
+++ client/src/components/Landing/LandingRight.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import { Typography } from "@mui/material";
+import Button from "../Button";
+import { Navigate, useNavigate } from "react-router-dom";
 function LandingRight() {
+  const navigate = useNavigate();
   return (
     <Grid
       container
-      style={{ backgroundColor: "#3aa9ab" }}
+      style={{ backgroundColor: "#4333a1" }}
       display="flex"
+      flexDirection="column"
       justifyContent="center"
       alignContent="center"
       height="100vh"
     >
-      <Typography
+      {/* <Typography
         sx={{
           fontSize: "4rem",
           fontWeight: "600",
@@ -21,10 +25,15 @@ function LandingRight() {
         }}
       >
         Hello, Friend!
-      </Typography>
+      </Typography> */}
       <Typography sx={{ fontSize: "1rem", color: "white" }}>
         Enter with your personal details
       </Typography>
+      <Button
+        text="Signup"
+        propClassName={"secondary__btn"}
+        onClick={navigate("/signup")}
+      />
     </Grid>
   );
 }
