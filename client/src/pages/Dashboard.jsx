063ff810538aaae64a011c8805e4eded0515de91
Mohamed <mhassankd35@gmail.com>
2022-12-10 08:08:00 -0600

    adds primary card function

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,18 +1,57 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Sidebar from "../components/Sidebar/Sidebar";
 import Grid from "@mui/material/Grid";
 import RightSideBar from "../components/Sidebar/RightSideBar";
+import Card from "../components/Dashboard/Card";
+import CardForm from "../components/Dashboard/CardForm";
+import BasicLayout from "../components/BasicLayout";
+import useFetch from "../hook/useFetch";
+import axios from "axios";
 function Dashboard() {
+  const [primaryCard, setPrimarCard] = useState({});
+
+  const [loading, setLoading] = useState(true);
+  // const { data, loading, error } = useFetch("/api/cards/primary");
+  // console.log("DATA", data);
+  // setPrimarCard(...data);
+
+  useEffect(() => {
+    const getPrimaryCard = async () => {
+      // setLoading(true);
+      try {
+        const response = await axios.get("/api/cards/primary");
+        // console.log("...", response.data);
+        setPrimarCard(...response.data);
+        setLoading(false);
+        // console.log("---------", response);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPrimaryCard();
+  }, []);
+
+  // console.log("///", primaryCard);
+  console.log("loading", loading);
   return (
-    <Grid container item md={12} direction="row">
-      <Grid item md={2}>
-        <Sidebar />
+    <BasicLayout>
+      <Grid item md={5}>
+        {loading ? (
+          <div>Please wait for data</div>
+        ) : (
+          <Card
+            number={primaryCard.number}
+            name={primaryCard.name}
+            expiry={primaryCard.expiry}
+            cvc={primaryCard.cvc}
+            // preview={false}
+          />
+        )}
+
+        {/* <CardForm /> */}
       </Grid>
-      <Grid item md={8}></Grid>
-      <Grid item md={2}>
-        <RightSideBar />
-      </Grid>
-    </Grid>
+    </BasicLayout>
   );
 }
 
