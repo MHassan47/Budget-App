06c3f56ec4afe13289918e423573c0208069ffa9
Mohamed <mhassankd35@gmail.com>
2022-12-10 08:06:19 -0600

    adds card  controller functions

---
+++ server/controllers/cardControllers.js
@@ -0,0 +1,37 @@
+const User = require("../models/userModel");
+const Card = require("../models/cardModel");
+
+const addCard = async (req, res) => {
+  const { number, name, expiry, cvc } = req.body;
+  //   console.log(req.body);
+  const user_id = req.user;
+  try {
+    const addCard = await Card.create({
+      user: user_id,
+      number,
+      name,
+      expiry,
+      cvc,
+    });
+    res.status(200).json(addCard);
+  } catch (err) {
+    res.status(401).json({ message: err });
+  }
+};
+
+const getPrimaryCard = async (req, res) => {
+  const user_id = req.user;
+  console.log(":::", user_id);
+  try {
+    const primaryCard = await Card.find({ user: user_id });
+    console.log("....", primaryCard);
+    res.json(primaryCard);
+  } catch (err) {
+    res.status(401).json({ message: err });
+  }
+};
+
+module.exports = {
+  addCard,
+  getPrimaryCard,
+};
