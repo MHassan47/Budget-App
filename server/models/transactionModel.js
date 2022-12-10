6be3dc97cbd4eb9363b42c988d3fccea8be1194e
Mohamed <mhassankd35@gmail.com>
2022-12-10 08:06:57 -0600

    creates transaction  schema

---
+++ server/models/transactionModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Transaction", transactionSchema);
