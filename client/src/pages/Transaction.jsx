117a4c1988bfe3385f7755913983103a53d097d1
Mohamed <mhassankd35@gmail.com>
2022-12-13 05:30:08 -0600

    creates transaction form page

---
+++ client/src/pages/Transaction.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BasicLayout from "../components/BasicLayout";
+import TransactionForm from "../components/Dashboard/TransactionForm";
+function Transaction() {
+  return (
+    <BasicLayout>
+      <TransactionForm />
+    </BasicLayout>
+  );
+}
+
+export default Transaction;
