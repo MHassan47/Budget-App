b1b305b230c88e7e5e55056a7978cf5d92abba87
Mohamed <mhassankd35@gmail.com>
2022-12-13 05:10:36 -0600

    adds expenses calculation logic

---
+++ client/src/components/Summary/Expenses.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Grid from "@mui/material/Grid";
+import ProgressBar from "./ProgressBar";
+function Expenses() {
+  const [currentExpenses, setCurrentExpenses] = useState();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const getExpenses = async () => {
+      // setLoading(true);
+      try {
+        const response = await axios.get("/api/transactions/");
+        let totalExpenses = 0;
+        const getData = await response.data.map((transaction) => {
+          if (transaction.amount < 0) {
+            totalExpenses += transaction.amount;
+          }
+        });
+        setCurrentExpenses(Math.abs(totalExpenses));
+        setLoading(false);
+        // console.log("---------", response);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getExpenses();
+  }, []);
+
+  return (
+    <Grid container item md={12} style={{}}>
+      {loading ? (
+        <div>Loading</div>
+      ) : (
+        <Grid item md={4}>
+          <ProgressBar currentExpenses={currentExpenses} />
+        </Grid>
+      )}
+    </Grid>
+  );
+}
+
+export default Expenses;
