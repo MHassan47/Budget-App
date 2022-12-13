4b074c66577f7d6354c4ffd4de03a048634ab9b0
Mohamed <mhassankd35@gmail.com>
2022-12-13 05:32:15 -0600

    adds preview route to transaction

--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const { verifyJWT } = require("../Middleware/authMiddleware");
 const {
   addTransaction,
+  getPreviewTransactions,
   getAllTransactions,
 } = require("../controllers/transactionControllers");
 
 router.post("/add", verifyJWT, addTransaction);
+router.get("/preview", verifyJWT, getPreviewTransactions);
 router.get("/", verifyJWT, getAllTransactions);
 
 module.exports = router;
