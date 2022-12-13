e3449b4fb873c462cb37ec3d25f499a7db376b0d
Mohamed <mhassankd35@gmail.com>
2022-12-13 05:13:42 -0600

    refactors dashboard ui

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -8,47 +8,97 @@ import CardForm from "../components/Dashboard/CardForm";
 import BasicLayout from "../components/BasicLayout";
 import useFetch from "../hook/useFetch";
 import axios from "axios";
+import IncomeChart from "../components/Dashboard/IncomeChart";
+import Expenses from "../components/Summary/Expenses";
 function Dashboard() {
-  const [primaryCard, setPrimarCard] = useState({});
-
-  const [loading, setLoading] = useState(true);
-  // const { data, loading, error } = useFetch("/api/cards/primary");
-  // console.log("DATA", data);
-  // setPrimarCard(...data);
-
-  useEffect(() => {
-    const getPrimaryCard = async () => {
-      // setLoading(true);
-      try {
-        const response = await axios.get("/api/cards/primary");
-        // console.log("...", response.data);
-        setPrimarCard(...response.data);
-        setLoading(false);
-        // console.log("---------", response);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getPrimaryCard();
-  }, []);
-
   // console.log("///", primaryCard);
-  console.log("loading", loading);
+
   return (
     <BasicLayout>
-      <Grid item md={5}>
-        {loading ? (
-          <div>Please wait for data</div>
-        ) : (
-          <Card
-            number={primaryCard.number}
-            name={primaryCard.name}
-            expiry={primaryCard.expiry}
-            cvc={primaryCard.cvc}
-            // preview={false}
-          />
-        )}
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignContent="center"
+        md={12}
+      >
+        {/* <Grid
+          item
+          md={4}
+          style={{
+            marginTop: "8vh",
 
+            backgroundColor: "white",
+            boxShadow: "10px 10px 18px rgba(60, 64, 67, 0.4)",
+            borderRadius: "8%",
+          }}
+        >
+          {loading ? (
+            <div>Please wait for data</div>
+          ) : (
+            <Card
+              number={primaryCard.number}
+              name={primaryCard.name}
+              expiry={primaryCard.expiry}
+              cvc={primaryCard.cvc}
+              // preview={false}
+            />
+          )}
+        </Grid> */}
+        {/* <Grid item md={2}></Grid> */}
+        <Grid
+          item
+          md={10}
+          justifyContent="center"
+          style={{
+            marginTop: "8vh",
+            // border: "1px solid black",
+            backgroundColor: "white",
+            boxShadow: "10px 10px 18px rgba(60, 64, 67, 0.4)",
+            borderRadius: "40px",
+            padding: "0.5rem",
+          }}
+        >
+          <Grid
+            item
+            style={{
+              fontSize: "1.7rem",
+              fontWeight: "600",
+              margin: "1rem",
+              color: "#290c64",
+            }}
+          >
+            Account Details
+          </Grid>
+          <IncomeChart />
+        </Grid>
+        <Grid
+          container
+          item
+          md={10}
+          style={{
+            marginTop: "8vh",
+            // border: "1px solid black",
+            height: "27vh",
+            backgroundColor: "white",
+            boxShadow: "10px 10px 18px rgba(60, 64, 67, 0.4)",
+            borderRadius: "40px",
+            padding: "0.5rem",
+          }}
+        >
+          <Grid
+            item
+            style={{
+              fontSize: "1.7rem",
+              fontWeight: "600",
+              margin: "1rem",
+              color: "#290c64",
+            }}
+          >
+            Expenses Summary
+          </Grid>
+          <Expenses />
+        </Grid>
         {/* <CardForm /> */}
       </Grid>
     </BasicLayout>
