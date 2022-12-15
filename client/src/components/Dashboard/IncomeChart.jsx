ae106855dd364b18b1e31016692bf2d30696098b
Mohamed <mhassankd35@gmail.com>
2022-12-15 02:16:40 -0600

    creates logic that dynamically renders transaction data on line chart

--- client/src/components/Dashboard/IncomeChart.jsx
+++ client/src/components/Dashboard/IncomeChart.jsx
@@ -4,51 +4,93 @@ import axios from "axios";
 import Grid from "@mui/material/Grid";
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
-
-// const gradient = ctx.createLinearGradient(0, 0, 0, 400);
-// gradient.addColorStop(0, "rgba(250,174,50,1)");
-// gradient.addColorStop(1, "rgba(250,174,50,0)");
-
-const data = {
-  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-  datasets: [
-    {
-      label: "Income",
-      data: [33, 53, 85, 41, 44, 65, 23, 15, 34, 13],
-      fill: false,
-      backgroundColor: "rgba(75,192,192,0.2)",
-      borderColor: "rgba(75,192,192,1)",
-      tension: 0.3,
-    },
-    {
-      label: "Expenses",
-      data: [33, 25, 35, 51, 54, 76],
-      fill: false,
-      borderColor: "#ff8789",
-      tension: 0.3,
-    },
-  ],
-};
+import { useState } from "react";
 
 function IncomeChart() {
+  const [monthlyExpenses, setMonthlyExpenses] = useState([]);
   useEffect(() => {
     const getMonthlyIncome = async () => {
       // setLoading(true);
       try {
-        const response = await axios.get("/api/transactions/");
+        const response = await axios.get("/api/transactions/monthly");
 
-        // setAllTransactions(response.data);
+        setMonthlyExpenses(response.data);
         // setLoading(false);
-        console.log("---------", response);
-        const getData = await response.data.map((transaction) =>
-          console.log(transaction.amount)
-        );
+
+        // const getData = await response.data.map((transaction) =>
+
+        // );
       } catch (err) {
         console.log(err);
       }
     };
     getMonthlyIncome();
   }, []);
+
+  const expensesData = {};
+  const incomeData = {};
+  const monthArray = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  monthlyExpenses.map((month) => {
+    if (month._id.type === "purchase") {
+      monthArray.map((item, i) => {
+        if (i === month._id.month - 1) {
+          expensesData[item] = -month.amount;
+        } else {
+          expensesData[item] = 0;
+        }
+      });
+    } else {
+      monthArray.map((item, i) => {
+        if (i === month._id.month - 1) {
+          incomeData[item] = month.amount;
+        } else {
+          incomeData[item] = 0;
+        }
+      });
+    }
+  });
+
+  // monthlyExpenses.map((month) => {
+  //   if (month._id.type === "purchase") {
+  //     expensesData[[month._id.day]] = -month.amount;
+  //   } else {
+  //     incomeData[[month._id.day]] = month.amount;
+  //   }
+  // });
+
+  const data = {
+    datasets: [
+      {
+        label: "Income",
+        data: incomeData,
+        fill: false,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)",
+        tension: 0.3,
+      },
+      {
+        label: "Expenses",
+        data: expensesData,
+        fill: false,
+        borderColor: "#ff8789",
+        tension: 0.3,
+      },
+    ],
+  };
 
   return (
     <Grid container style={{ marginTop: "4vh" }}>
