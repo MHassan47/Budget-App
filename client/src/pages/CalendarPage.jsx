e53ce5c0fd6b754e87e85f2452320c7c853cc7ef
Mohamed <mhassankd35@gmail.com>
2022-12-15 02:24:25 -0600

    creates calendar component

---
+++ client/src/pages/CalendarPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BasicLayout from "../components/BasicLayout";
+import Calendar from "../components/Calendar/Index";
+function CalendarPage() {
+  return (
+    <BasicLayout>
+      <Calendar />
+    </BasicLayout>
+  );
+}
+
+export default CalendarPage;
