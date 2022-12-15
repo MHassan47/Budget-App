a228a20709da72c5da04ef49b262fff574669a5e
Mohamed <mhassankd35@gmail.com>
2022-12-15 02:33:09 -0600

    creates calendar schema

---
+++ server/models/CalendarModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const calendarSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    start: {
+      type: Date,
+      required: true,
+    },
+    end: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Calendar", calendarSchema);
