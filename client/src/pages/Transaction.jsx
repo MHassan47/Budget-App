3486107febd258cc5e4a5d8ad7679deb26806fd5
Mohamed <mhassankd35@gmail.com>
2022-12-15 02:25:10 -0600

    changes transaction component content

--- client/src/pages/Transaction.jsx
+++ client/src/pages/Transaction.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import BasicLayout from "../components/BasicLayout";
+import AllTransactions from "../components/Dashboard/AllTransactions";
 import TransactionForm from "../components/Dashboard/TransactionForm";
 function Transaction() {
   return (
     <BasicLayout>
-      <TransactionForm />
+      <AllTransactions />
     </BasicLayout>
   );
 }
