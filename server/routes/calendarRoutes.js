0093ae9d8e12e2f8f74a9fd68c68992a9be67e2f
Mohamed <mhassankd35@gmail.com>
2022-12-15 02:33:18 -0600

    adds calendar route

---
+++ server/routes/calendarRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const { verifyJWT } = require("../Middleware/authMiddleware");
+const { addEvent, getEvent } = require("../controllers/calendarControllers");
+
+router.post("/add", verifyJWT, addEvent);
+router.get("/get", verifyJWT, getEvent);
+// router.get("/", verifyJWT, getAllCard);
+
+module.exports = router;
