2febdd9cdb44ce0ad7aa37a1450c4d33b32f8f76
Mohamed <mhassankd35@gmail.com>
2022-12-16 14:28:18 -0600

    creates logout button and logic

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Grid from "@mui/material/Grid";
 import { SidebarData } from "./SidebarData";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../../features/user/userSlice";
 import classes from "../Styles/Sidebar.module.scss";
 import { useNavigate } from "react-router-dom";
+
 function Sidebar() {
+  const [selected, setSelected] = useState("/dashboard");
+  const { isSuccess } = useSelector((state) => state.auth);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
+
+  console.log(window.location.pathname);
   return (
     <Grid
       // direction="column"
-      // display="flex"
+
       // style={{ background: "#4333a1" }}
       className={`${classes.container}`}
     >
@@ -18,8 +33,10 @@ function Sidebar() {
             <li
               key={key}
               className="row"
-              id={window.location.pathname === val.link ? "active" : ""}
+              id={val.link === selected ? "active" : ""}
               onClick={() => {
+                console.log(selected === val.link);
+                setSelected(val.link);
                 navigate(val.link);
               }}
             >
@@ -28,6 +45,10 @@ function Sidebar() {
             </li>
           );
         })}
+
+        <button className={`${classes.logout_btn}`} onClick={logoutHandler}>
+          <LogoutIcon /> Logout
+        </button>
       </ul>
     </Grid>
   );
