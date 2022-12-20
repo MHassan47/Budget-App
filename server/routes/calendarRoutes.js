fbc09e1e1f1b66b3021ca6d09d0fe27bfe5a45c0
Mohamed <mhassankd35@gmail.com>
2022-12-20 17:55:21 -0600

    creates delete calendar event request route handler

--- server/routes/calendarRoutes.js
+++ server/routes/calendarRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { verifyJWT } = require("../Middleware/authMiddleware");
-const { addEvent, getEvent } = require("../controllers/calendarControllers");
+const {
+  addEvent,
+  getEvent,
+  deleteEvent,
+} = require("../controllers/calendarControllers");
 
 router.post("/add", verifyJWT, addEvent);
 router.get("/", verifyJWT, getEvent);
-// router.get("/", verifyJWT, getAllCard);
+router.delete("/delete/:id", verifyJWT, deleteEvent);
 
 module.exports = router;
