70334f937d217c52b8d496ac7fadab755d70e338
Mohamed <mhassankd35@gmail.com>
2022-12-20 17:53:31 -0600

    creates redux logic handling update user state

--- client/src/features/user/userSlice.js
+++ client/src/features/user/userSlice.js
@@ -46,6 +46,23 @@ export const logout = createAsyncThunk("auth/logout", async () => {
   await authService.logout();
 });
 
+export const updateUser = createAsyncThunk(
+  "auth/updateUser",
+  async (user, thunkAPI) => {
+    try {
+      return await authService.updateUser(user);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 export const userSlice = createSlice({
   name: "auth",
   initialState,
@@ -86,6 +103,20 @@ export const userSlice = createSlice({
         state.isError = true;
         state.message = action.payload;
         state.user = null;
+      })
+      .addCase(updateUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(updateUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(updateUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        // state.user = null;
       })
       .addCase(logout.fulfilled, (state) => {
         state.user = null;
