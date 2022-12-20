492e0c610773bdaae1c1382b92412019c50730ba
Mohamed <mhassankd35@gmail.com>
2022-12-20 17:53:24 -0600

    creates redux logic handling update user state

--- client/src/features/user/userService.js
+++ client/src/features/user/userService.js
@@ -1,11 +1,26 @@
 import axios from "axios";
-
+import FormData from "form-data";
 const API_URL = "/api/users/";
 
 // Register user
 const register = async (userData) => {
-  const response = await axios.post(API_URL + "register", userData);
+  const formData = new FormData();
+  // console.log("userdata", userData.profilePicture);
+  // ...
 
+  formData.append("firstName", userData.firstName);
+  formData.append("lastName", userData.lastName);
+  formData.append("email", userData.email);
+  formData.append("password", userData.password);
+  formData.append("profilePicture", JSON.stringify(userData.profilePicture));
+
+  const response = await axios.post(API_URL + "register", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  console.log(response);
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
     localStorage.setItem("accessToken", response.data.token);
@@ -35,10 +50,22 @@ const logout = () => {
   localStorage.removeItem("login");
 };
 
+const updateUser = async (userData) => {
+  const response = await axios.put(API_URL + "update", userData);
+
+  if (response.data) {
+    localStorage.removeItem("user");
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
 const authService = {
   register,
   logout,
   login,
+  updateUser,
 };
 
 export default authService;
