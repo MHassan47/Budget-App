f34d81dd95307d38bde23e783989693fe1f7d8a5
Mohamed <mhassankd35@gmail.com>
2022-12-20 17:53:52 -0600

    creates profile page and imports components

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import { Grid } from "@mui/material";
+import React, { useState } from "react";
 import BasicLayout from "../components/BasicLayout";
+import ProfileDetails from "../components/Profile/ProfileDetails";
+import ProfileHeader from "../components/Profile/ProfileHeader";
 
 function Profile() {
-  return <BasicLayout></BasicLayout>;
+  const [currentTab, setCurrentTab] = useState("profile");
+  return (
+    <BasicLayout>
+      <Grid
+        container
+        display="flex"
+        justifyContent="center"
+        md={12}
+        // direction="column"
+        style={{ marginTop: "5vh" }}
+      >
+        <Grid item md={10}>
+          <ProfileHeader />
+        </Grid>
+        <Grid
+          container
+          item
+          // display="flex"
+          // justifyContent="center"
+          md={10}
+          style={{ marginTop: "5vh" }}
+        >
+          <ProfileDetails />
+        </Grid>
+      </Grid>
+    </BasicLayout>
+  );
 }
 
 export default Profile;
